1ab9db4605f1402774c1aae81cc69e80debfb2ab
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2021-12-31 17:13:18 +0900

    import liker

--- hello_nerves_poncho_firmware/config/target.exs
+++ hello_nerves_poncho_firmware/config/target.exs
@@ -112,3 +112,15 @@ config :mdns_lite,
 config :hello_nerves_poncho_line_bandit,
   channel_secret: System.get_env("LINE_CHANNEL_SECRET"),
   channel_access_token: System.get_env("LINE_CHANNEL_ACCESS_TOKEN")
+
+config :extwitter, :oauth,
+  consumer_key: System.get_env("STSP_TWITTER_CONSUMER_KEY"),
+  consumer_secret: System.get_env("STSP_TWITTER_CONSUMER_SECRET"),
+  access_token: System.get_env("STSP_TWITTER_ACCESS_TOKEN"),
+  access_token_secret: System.get_env("STSP_TWITTER_ACCESS_TOKEN_SECRET")
+
+config :liker, Liker.Scheduler,
+  jobs: [
+    # Every 15 minutes
+    {"*/15 * * * *", {Liker, :run, []}}
+  ]
