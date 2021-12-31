d5ada78c86a5463c539930e1a144b2dd0b2a099b
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2021-12-31 15:13:20 +0900

    mix deps.get

--- liker/mix.exs
+++ liker/mix.exs
@@ -24,6 +24,10 @@ defmodule Liker.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:oauther, "~> 1.3"},
+      {:jason, "~> 1.3"},
+      {:extwitter, "~> 0.13.0"},
+      {:quantum, "~> 3.4"}
     ]
   end
 end
