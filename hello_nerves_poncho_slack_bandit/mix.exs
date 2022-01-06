565d3340e917ab6506e3106766061f6b7d648b05
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2022-01-07 03:48:58 +0900

    mix deps.get

--- hello_nerves_poncho_slack_bandit/mix.exs
+++ hello_nerves_poncho_slack_bandit/mix.exs
@@ -24,6 +24,10 @@ defmodule HelloNervesPonchoSlackBandit.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:bandit, "~> 0.4.5"},
+      {:httpoison, "~> 1.8"},
+      {:jason, "~> 1.3"},
+      {:aht20, "~> 0.4.2"}
     ]
   end
 end
